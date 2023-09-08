2b798377a8b2f3227a20202eb2999701e6ce4c69
paulrus <pgriswold@fusiondigitalproductions.com>
2023-09-08 11:53:18 -0500

    Update imapconnection.py

--- imapconnection.py
+++ imapconnection.py
@@ -1,6 +1,22 @@
 import imaplib
 
-imap = imaplib.IMAP4_SSL("imap.example.com")
-imap.login("username@example.com", "password")
+imap = imaplib.IMAP4_SSL("imap.server.com")
+imap.login("user@email.com", "password")
 
 imap.select("INBOX")
+
+# Search for emails from specific sender
+status, data = imap.search(None, 'FROM "sender@email.com"') 
+
+for num in data[0].split():
+    # Fetch email by ID
+    status, data = imap.fetch(num, '(RFC822)')
+    
+    # Parse email content
+    email = parse_email(data[0][1]) 
+    
+    # Apply rules
+    label = apply_rules(email)
+    
+    # Update email flags on server
+    imap.store(num, '+FLAGS', '\\Important')
